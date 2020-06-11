04b1c80a4d0651eaf371cd0882c5fd412b9bd62b
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-12 05:06:44 +0530

    Completed UC2 Calculate Daily Wage

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -1,14 +1,16 @@
+import java.util.*;
 public class EmployeeBuilder {
 	public static void main(String args[]) {
-		System.out.println("Welcome to Employee Wage Compilation Problem.");
+		int ratePerHr=20,empHr=0;
+		System.out.println("Welcome to Employee Wage Computation Program");
 		int full_time=1;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if (empCheck==full_time){
-			System.out.println("Present");
-			}
-		else
-			System.out.println("Absent");
-
-		}
+			empHr=8;
+		    }
+		else {
+			empHr=0;
+		    }
+		System.out.println("DailyWage="+ratePerHr*empHr);
 	}
 }
