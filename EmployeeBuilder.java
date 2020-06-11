dedec305157c8ca0f1dbd733c2a53dd7e25b94aa
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-12 05:15:44 +0530

    Completed UC3 Add Part time Employee and Wage

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -4,9 +4,14 @@ public class EmployeeBuilder {
 		int ratePerHr=20,empHr=0;
 		System.out.println("Welcome to Employee Wage Computation Program");
 		int full_time=1;
-		double empCheck=Math.floor(Math.random()*10)%2;
+		int part_time=2;
+		double empCheck=Math.floor(Math.random()*10)%3;
 		if (empCheck==full_time){
 			empHr=8;
+		    }
+		else if(empCheck==2){
+			empHr=8;
+
 		    }
 		else {
 			empHr=0;
