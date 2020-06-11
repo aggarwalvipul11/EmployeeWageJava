a11ce1096e24ecd9d566b7d62ab55a8eb90141fe
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-12 04:51:32 +0530

    Complete Welcome Message in Master Branch

---
+++ EmployeeBuilder.java
@@ -0,0 +1,8 @@
+public class EmployeeBuilder {
+	public static void main(String args[]) {
+		/*
+			Print the Welcome Message.
+		*/
+		System.out.println("Welcome to Employee Wage Compilation Problem.");
+	}
+}
