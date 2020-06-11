c02ba26052dc6ca45b0ddf70115afacb7d16c8f3
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-12 05:26:49 +0530

    Completed UC4 Solving using switch case statement

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -6,16 +6,13 @@ public class EmployeeBuilder {
 		int full_time=1;
 		int part_time=2;
 		double empCheck=Math.floor(Math.random()*10)%3;
-		if (empCheck==full_time){
-			empHr=8;
-		    }
-		else if(empCheck==2){
-			empHr=8;
-
-		    }
-		else {
-			empHr=0;
-		    }
+		switch((int)empCheck) {
+		case 1: empHr=8;
+		break;
+		case 2: empHr=8;
+		break;
+		default:empHr=0;
+		}
 		System.out.println("DailyWage="+ratePerHr*empHr);
 	}
 }
