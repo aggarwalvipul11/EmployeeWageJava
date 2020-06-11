fe7c544ab036fe9893dc3c21ccae4f063c827963
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-12 04:57:34 +0530

    Complete UC1 (If employee is present or abscent).

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -1,8 +1,14 @@
 public class EmployeeBuilder {
 	public static void main(String args[]) {
-		/*
-			Print the Welcome Message.
-		*/
 		System.out.println("Welcome to Employee Wage Compilation Problem.");
+		int full_time=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if (empCheck==full_time){
+			System.out.println("Present");
+			}
+		else
+			System.out.println("Absent");
+
+		}
 	}
 }
