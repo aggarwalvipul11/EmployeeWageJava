8b6972645d6ee716e88ed13221893733903f9a6d
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-12 05:40:39 +0530

    Completed UC6 Calculating Wages till a condition of total working hours or days in a month.

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -1,21 +1,22 @@
-import java.util.*;
-public class EmployeeBuilder {
-	public static void main(String args[]) {
-		int ratePerHr=20,empHr=0,Total_hrs=0;
+public class EmployeeWageUC1 {
+
+	public static void main(String[] args) {
+		int ratePerHr=20,empHr=0,Total_hrs=0,Total_days=0;
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int full_time=1,part_time=2,Working_days=20;
-		for (int i=0;i<Working_days;i++) {
-			double empCheck=Math.floor(Math.random()*10)%3;
-			switch((int)empCheck) {
-				case 1: empHr=8;
-				break;
-				case 2: empHr=8;
-				break;
-				default:empHr=0;
-			}	
-		System.out.println("DailyWage="+ratePerHr*empHr);
-		Total_hrs+=empHr;
-		}
-		System.out.println("Monthly Wage="+(ratePerHr*Total_hrs));
-	}
+		int full_time=1,part_time=2,Working_days=20,max_hrs=100;
+			while(Total_hrs<max_hrs && Total_days<Working_days) {
+				Total_days++;
+				double empCheck=Math.floor(Math.random()*10)%3;
+				switch((int)empCheck) {
+					case 1: empHr=8;
+					break;
+					case 2: empHr=4;
+					break;
+					default:empHr=0;
+				}	
+			Total_hrs+=empHr;
+			}
+		System.out.println("Total Salary="+(ratePerHr*Total_hrs));
+	 }
+
 }
