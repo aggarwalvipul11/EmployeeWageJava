314e3518175dd6f4ddbc7ffceb10da54b6c197ae
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-12 05:34:49 +0530

    Completed UC5 Calculating Wages for a Month

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -1,18 +1,21 @@
 import java.util.*;
 public class EmployeeBuilder {
 	public static void main(String args[]) {
-		int ratePerHr=20,empHr=0;
+		int ratePerHr=20,empHr=0,Total_hrs=0;
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int full_time=1;
-		int part_time=2;
-		double empCheck=Math.floor(Math.random()*10)%3;
-		switch((int)empCheck) {
-		case 1: empHr=8;
-		break;
-		case 2: empHr=8;
-		break;
-		default:empHr=0;
-		}
+		int full_time=1,part_time=2,Working_days=20;
+		for (int i=0;i<Working_days;i++) {
+			double empCheck=Math.floor(Math.random()*10)%3;
+			switch((int)empCheck) {
+				case 1: empHr=8;
+				break;
+				case 2: empHr=8;
+				break;
+				default:empHr=0;
+			}	
 		System.out.println("DailyWage="+ratePerHr*empHr);
+		Total_hrs+=empHr;
+		}
+		System.out.println("Monthly Wage="+(ratePerHr*Total_hrs));
 	}
 }
