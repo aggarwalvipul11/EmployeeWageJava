c0d091b2a53200d4182a130de9f1c12bb356c8f2
aggarwalvipul11 <65864933+aggarwalvipul11@users.noreply.github.com>
2020-06-17 08:12:01 +0530

    Update EmpWageBuilderUC8
    Completed UC8

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -1,24 +1,100 @@
-  
-import java.util.*;
-public class EmployeeWageUC1 {
-
-	public static void main(String[] args) {
-		int ratePerHr=20,empHr=0,Total_hrs=0,Total_days=0;
-		System.out.println("Welcome to Employee Wage Computation Program");
-		int full_time=1,part_time=2,Working_days=20,max_hrs=100;
-			while(Total_hrs<max_hrs && Total_days<Working_days) {
-				Total_days++;
-				double empCheck=Math.floor(Math.random()*10)%3;
-				switch((int)empCheck) {
-					case 1: empHr=8;
-					break;
-					case 2: empHr=4;
-					break;
-					default:empHr=0;
-				}	
-			Total_hrs+=empHr;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+class Company{
+	private String compName;
+	private int ratePerHr;
+	private int maxWorkingDays;
+	private int maxWorkingHrs;
+	private int totalDays=0;
+	private ArrayList<Integer> salary=new ArrayList<>();
+	private ArrayList<Integer> totalSalary=new ArrayList<>();
+	public Company(String compName,int ratePerHr,int maxWorkingDays,int maxWorkingHrs) {
+		this.compName=compName;
+		this.ratePerHr=ratePerHr;
+		this.maxWorkingDays=maxWorkingDays;
+		this.maxWorkingHrs=maxWorkingHrs;
+	}
+	public Company(String compName,ArrayList salary,ArrayList totalSalary,int totalDays) {
+		this.salary=salary;
+		this.totalSalary=totalSalary;
+		this.totalDays=totalDays;
+		this.compName=compName;
+	}
+	public String getCompName() {
+		return compName;
+	}
+	public int getRatePerHr() {
+		return ratePerHr;
+	}
+	public int getMaxWorkingDays() {
+		return maxWorkingDays;
+	}
+	public int getMaxWorkingHrs() {
+		return maxWorkingHrs;
+	}
+	public int getTotalDays() {
+		return totalDays;
+	}
+	public ArrayList<Integer> getSalary() {
+		return salary;
+	}
+	public ArrayList<Integer> getTotalSalary() {
+		return totalSalary;
+	}
+	
+}
+class Employeesalary{
+	public Company salary(Company C) {
+		int empHr=0,totalHrs=0,dailyWage=0,diff=0,sum=0,totalDays=0;
+		ArrayList<Integer> salary=new ArrayList<>();
+		ArrayList<Integer> totalSalary=new ArrayList<>();
+		Random rand=new Random();
+		while(totalDays<=C.getMaxWorkingDays() && totalHrs<=C.getMaxWorkingHrs()) {
+			totalDays++;
+			int empCheck=rand.nextInt(3);
+			switch(empCheck) {
+				case 1: empHr=8;
+						break;
+				case 2: empHr=4;
+						break;
+				default: empHr=0;
 			}
-		System.out.println("Total Salary="+(ratePerHr*Total_hrs));
+			totalHrs+=empHr;
+			if(totalHrs>C.getMaxWorkingHrs()) {
+				diff=totalHrs-C.getMaxWorkingHrs();
+				empHr-=diff;
+			}
+		dailyWage=C.getRatePerHr()*empHr;
+		salary.add(dailyWage);
+		sum+=dailyWage;
+		totalSalary.add(sum);
+		}
+		return new Company(C.getCompName(),salary,totalSalary,totalDays);
+	}
+	public void disp(Company C) {
+		System.out.println(C.getCompName()+"'s salary details for the month are as below:");
+		System.out.println("Day\tDaily Wage\tTotal Salary");
+		for(int i=1;i<C.getTotalDays();i++) {
+			System.out.println(i+"\t"+C.getSalary().get(i)+"\t\t"+C.getTotalSalary().get(i));
+			}
+	}
+}
+public class EmployeeBuilder {
+	System.out.println("Welcome to Employee Wage Computation");
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter Company name");
+	String name=sc.next();
+	System.out.println("Enter wage per hour");
+	int ratePerHr=sc.nextInt();
+	System.out.println("Enter maximum working days of a month");
+	int workingDays=sc.nextInt();
+	System.out.println("Enter maximum working hours of a month");
+	int workingHrs=sc.nextInt();
+	Company company=new Company(name,ratePerHr,workingDays,workingHrs);
+	Employeesalary empsal=new Employeesalary();
+	company=empsal.salary(company);
+	empsal.disp(company);
 	 }
 
 }
